a76f534fdfdc12e113ceeca73ebb992945a10e8b
felipematheus1337 <lipehbr@gmail.com>
2026-06-09 21:57:09 -0300

    Criado docker-compose com Postgres, criado classe de configuração para inicialização do Banco de dados.

---
+++ internal/config/config.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Init() error {
+
+	var err error
+
+	db, err = InitializePostgres()
+
+	if err != nil {
+		return fmt.Errorf("error initializing database: %v", err)
+	}
+
+	return nil
+}
