c2d0b3cb633f15cb42807c19d71e71d5b1f89ba7
felipematheus1337 <lipehbr@gmail.com>
2026-06-09 22:30:57 -0300

    Finalizado inicialização com main.go

--- main.go
+++ main.go
@@ -1,8 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/felipematheus1337/go-ecommerce-ms/internal/config"
+	"github.com/felipematheus1337/go-ecommerce-ms/internal/handler"
+	"github.com/felipematheus1337/go-ecommerce-ms/internal/repository"
+	"github.com/felipematheus1337/go-ecommerce-ms/internal/router"
+	"github.com/felipematheus1337/go-ecommerce-ms/internal/service"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 
-	fmt.Print("Ecommerce-Project")
+	err := config.Init()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	r := gin.Default()
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	db, err := config.InitializePostgres()
+
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+
+	repo := repository.NewProductRepository(db)
+
+	services := service.NewProductService(repo)
+
+	handler := handler.NewProductHandler(services)
+
+	router.InitializeRoutes(r, handler)
+
+	err = r.Run(":" + port)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
