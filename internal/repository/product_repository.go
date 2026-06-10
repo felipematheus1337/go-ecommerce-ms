e198a1c591f285e6272b400ece5cc146c0596b64
felipematheus1337 <lipehbr@gmail.com>
2026-06-09 22:24:52 -0300

    Finalizado camada de service com Create e GetAll, assim como Repository pattern.

--- internal/repository/product_repository.go
+++ internal/repository/product_repository.go
@@ -1,9 +1,22 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"github.com/felipematheus1337/go-ecommerce-ms/internal/model"
+	"gorm.io/gorm"
+)
 
 type ProductRepository struct {
 	db *gorm.DB
+}
+
+func (r ProductRepository) GetAll(products *[]model.Product) *gorm.DB {
+
+	return r.db.Find(products)
+}
+
+func (r ProductRepository) Create(m *model.Product) *gorm.DB {
+	return r.db.Create(m)
+
 }
 
 func NewProductRepository(db *gorm.DB) *ProductRepository {
--- internal/service/product_service.go
+++ internal/service/product_service.go
@@ -1,9 +1,55 @@
 package service
 
-import "github.com/felipematheus1337/go-ecommerce-ms/internal/repository"
+import (
+	"github.com/felipematheus1337/go-ecommerce-ms/internal/dto/response"
+	"github.com/felipematheus1337/go-ecommerce-ms/internal/model"
+	"github.com/felipematheus1337/go-ecommerce-ms/internal/repository"
+)
 
 type ProductService struct {
 	repo *repository.ProductRepository
+}
+
+func (s ProductService) Create(product model.Product) (response.ProductResponse, error) {
+	var productResponse *response.ProductResponse
+
+	if err := s.repo.Create(&product).Error; err != nil {
+		return response.ProductResponse{}, nil
+	}
+
+	productResponse = &response.ProductResponse{
+		ID:        product.ID,
+		Name:      product.Name,
+		Stock:     product.Stock,
+		CreatedAt: product.CreatedAt,
+		Price:     product.Price,
+	}
+
+	return *productResponse, nil
+
+}
+
+func (s ProductService) GetProducts() ([]response.ProductResponse, error) {
+	var products []model.Product
+
+	if err := s.repo.GetAll(&products).Error; err != nil {
+		return nil, err
+	}
+
+	var listProductResponse []response.ProductResponse
+
+	for _, emp := range products {
+		listProductResponse = append(listProductResponse, response.ProductResponse{
+			ID:        emp.ID,
+			Name:      emp.Name,
+			Price:     emp.Price,
+			Stock:     emp.Stock,
+			CreatedAt: emp.CreatedAt,
+		})
+	}
+
+	return listProductResponse, nil
+
 }
 
 func NewProductService(repo *repository.ProductRepository) *ProductService {
