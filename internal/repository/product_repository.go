eb3e6f848f696e5841358a6c1c970bd8743f2116
felipematheus1337 <lipehbr@gmail.com>
2026-06-09 21:59:56 -0300

    Iniciado camada de repositório, ainda sem os métodos.

---
+++ internal/repository/product_repository.go
@@ -0,0 +1,11 @@
+package repository
+
+import "gorm.io/gorm"
+
+type ProductRepository struct {
+	db *gorm.DB
+}
+
+func NewProductRepository(db *gorm.DB) *ProductRepository {
+	return &ProductRepository{db: db}
+}
